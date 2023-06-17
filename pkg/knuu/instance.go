e076b702b48fea688ec3a28d775d70ff9cf17a86
Ehsan Rahmani <ehsan.rahmani@divar.ir>
2023-06-17 22:09:00 +0330

    remove duplication

--- pkg/knuu/instance.go
+++ pkg/knuu/instance.go
@@ -596,24 +596,8 @@ func (i *Instance) Clone() (*Instance, error) {
 		return nil, fmt.Errorf("error generating k8s name for instance '%s': %w", i.name, err)
 	}
 	// Create a new instance with the same attributes as the original instance
-	return &Instance{
-		name:              i.name,
-		k8sName:           newK8sName,
-		imageName:         i.imageName,
-		state:             i.state,
-		instanceType:      i.instanceType,
-		kubernetesService: i.kubernetesService,
-		builderFactory:    i.builderFactory,
-		kubernetesPod:     i.kubernetesPod,
-		portsTCP:          i.portsTCP,
-		portsUDP:          i.portsUDP,
-		files:             i.files,
-		command:           i.command,
-		args:              i.args,
-		env:               i.env,
-		volumes:           i.volumes,
-		memoryRequest:     i.memoryRequest,
-		memoryLimit:       i.memoryLimit,
-		cpuRequest:        i.cpuRequest,
-	}, nil
+	ins := i.cloneWithSuffix("")
+	ins.k8sName = newK8sName
+	ins.instanceType = i.instanceType
+	return ins, nil
 }
