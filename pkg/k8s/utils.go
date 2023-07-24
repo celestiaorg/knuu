588dd7d6c9b68704f8b3e0d1d363d3814f026f31
Smuu <18609909+Smuu@users.noreply.github.com>
2023-07-24 10:43:24 +0200

    feat: move utils to utils.go
    Signed-off-by: Smuu <18609909+Smuu@users.noreply.github.com>

---
+++ pkg/k8s/utils.go
@@ -0,0 +1,19 @@
+package k8s
+
+import "time"
+
+// namespace is the current namespace in use by the Kubernetes client.
+var namespace = ""
+
+// timeout is the timeout for Kubernetes operations
+const timeout = 20 * time.Second
+
+// Namespace returns the current namespace in use.
+func Namespace() string {
+	return namespace
+}
+
+// setNamespace updates the namespace to the provided string.
+func setNamespace(newNamespace string) {
+	namespace = newNamespace
+}
