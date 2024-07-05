6f81e52ac90359641471d81cb1ab034fa3c4c4c9
sysrex <769991+sysrex@users.noreply.github.com>
2024-07-05 11:14:58 +0200

    refactor: change basic test to suite (#437)
    * refactor: change basic test to suite
    
    * refactor: change basic test to suite
    
    refactor: Change the Test suite for Basic for the time being without proxy and add more test cases
    
    * refactor: TestMain
    
    * refactor: changes for the test refactor
    
    * fix: typo
    
    * fix: typo
    
    * fixed
    
    * fix: trim spaces
    
    * fix: linter
    
    ---------
    
    Co-authored-by: smuu <18609909+smuu@users.noreply.github.com>
    Co-authored-by: Mojtaba <mojtaba@celestia.org>

---
+++ e2e/basic/suite_setup_test.go
@@ -0,0 +1,42 @@
+package basic
+
+import (
+	"context"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/celestiaorg/knuu/pkg/knuu"
+)
+
+type TestSuite struct {
+	suite.Suite
+	Knuu *knuu.Knuu
+}
+
+func (s *TestSuite) SetupSuite() {
+	var (
+		err error
+		ctx = context.Background()
+	)
+	s.Knuu, err = knuu.New(ctx, knuu.Options{
+		TestScope: "e2e-basic",
+	})
+	s.Require().NoError(err)
+	s.Knuu.HandleStopSignal(ctx)
+}
+
+func (s *TestSuite) TearDownSuite() {
+	s.T().Cleanup(func() {
+		logrus.Info("Tearing down test suite...")
+		err := s.Knuu.CleanUp(context.Background())
+		if err != nil {
+			s.T().Logf("Error cleaning up test suite: %v", err)
+		}
+	})
+}
+
+func TestRunSuite(t *testing.T) {
+	suite.Run(t, new(TestSuite))
+}
